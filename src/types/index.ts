4bcd8a40e38957a4661f648c669fabf6f4c8f227
KanybekovMaksat <maksatkanybekov2004@gmail.com>
2026-02-15 16:29:36 +0600

    fixed bugs and unified one temmplate and one course

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,47 @@
+// export interface Student {
+//   id: string;
+//   fullName: string;
+//   skill1: number;
+//   skill2: number;
+//   skill3: number;
+//   skill4: number;
+//   skill5: number;
+// }
+
+// export interface Course {
+//   id: string;
+//   name: string;
+//   skills: string[];
+//   templatePdf?: Uint8Array;
+// }
+
+// export const COURSES: Course[] = [
+//   {
+//     id: 'scratch',
+//     name: 'Scratch',
+//     skills: ['HTML/CSS', 'JavaScript', 'React', ]
+//   },
+//   {
+//     id: 'roblox',
+//     name: 'Roblox',
+//     skills: ['Python', 'Статистика', ]
+//   },
+//   {
+//     id: 'teens',
+//     name: 'Teens',
+//     skills: ['HTML', 'CSS']
+//   },
+//   {
+//     id: 'jst',
+//     name: 'JavaScript',
+//     skills: ['HTML&CSS', 'Javascript', 'Javascript PRO',]
+//   },
+//   {
+//     id: 'python',
+//     name: 'Python',
+//     skills: ['Python',]
+//   }
+// ];
 export interface Student {
   id: string;
   fullName: string;
@@ -17,28 +61,8 @@ export interface Course {
 
 export const COURSES: Course[] = [
   {
-    id: 'scratch',
-    name: 'Scratch',
-    skills: ['HTML/CSS', 'JavaScript', 'React', ]
+    id: 'course1',
+    name: 'Курс',
+    skills: ['HTML/CSS', 'JavaScript', 'React'],
   },
-  {
-    id: 'roblox',
-    name: 'Roblox',
-    skills: ['Python', 'Статистика', ]
-  },
-  {
-    id: 'teens',
-    name: 'Teens',
-    skills: ['HTML', 'CSS']
-  },
-  {
-    id: 'jst',
-    name: 'JavaScript',
-    skills: ['HTML&CSS', 'Javascript', 'Javascript PRO',]
-  },
-  {
-    id: 'python',
-    name: 'Python',
-    skills: ['Python',]
-  }
 ];
